ca10b6852768585351ebdfe5cfe3367752577379
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-07 16:28:39 +0200

    25 numeri ce li abbiamo

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 $(document).ready(function() {
 
-    
+    // Creare in HTML una griglia di 5x5 quadrati vuoti.
+    // Con jQuery, scrivere un numero random tra 0 e 10 dentro ognuno.
+    // BONUS 1: generare la griglia con jQuery
+    // BONUS 2: se il numero da inserire è pari scriverlo in rosso, se è dispari in nero, se è 0 in verde
+
+
+    //estraggo 25 numeri random tra 1 e 10
+    function genera_random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
+
+    var venticinque_numeri = [];
+
+    do {
+        var numero = genera_random(1,10);
+        venticinque_numeri.push(numero);
+
+    } while (venticinque_numeri.length < 25);
+
+    console.log(venticinque_numeri);
+
+    // li scrivo all'interno dei quadrati
+
+
+
 
 
 });
