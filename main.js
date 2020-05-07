2988ebe653bb26ceb020d7bd67d4d8895894978e
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-07 16:50:06 +0200

    each

--- main.js
+++ main.js
@@ -7,24 +7,32 @@ $(document).ready(function() {
 
 
     //estraggo 25 numeri random tra 1 e 10
+
+
+    // var venticinque_numeri = [];
+    //
+    // do {
+    //     var numero = genera_random(1,10);
+    //     // venticinque_numeri.push(numero);
+    //      $('.quadrato').text(numero);
+    //
+    // } while (venticinque_numeri.length < 25);
+    //
+    // console.log(venticinque_numeri);
+
+
+    // for (var i = 1; i < 25; i++) {
+    //     var numero = genera_random(1, 10);
+    //     $('.quadrato').text(numero);
+    // }
+
+    $('.quadrato').each(function(){
+            var numero = genera_random(1, 10);
+            $('.quadrato').text(numero);
+    })
+
     function genera_random(min, max) {
         return Math.floor(Math.random() * (max - min + 1) ) + min;
     }
-
-    var venticinque_numeri = [];
-
-    do {
-        var numero = genera_random(1,10);
-        venticinque_numeri.push(numero);
-
-    } while (venticinque_numeri.length < 25);
-
-    console.log(venticinque_numeri);
-
-    // li scrivo all'interno dei quadrati
-
-
-
-
 
 });
