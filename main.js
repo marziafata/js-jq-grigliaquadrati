2affc9023da0d911a1a597f7b04649af5af29af5
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-07 17:23:46 +0200

    aggiunto 0 verde

--- main.js
+++ main.js
@@ -6,30 +6,12 @@ $(document).ready(function() {
     // BONUS 2: se il numero da inserire è pari scriverlo in rosso, se è dispari in nero, se è 0 in verde
 
 
-    //estraggo 25 numeri random tra 1 e 10
-
-
-    // var venticinque_numeri = [];
-    //
-    // do {
-    //     var numero = genera_random(1,10);
-    //     // venticinque_numeri.push(numero);
-    //      $('.quadrato').text(numero);
-    //
-    // } while (venticinque_numeri.length < 25);
-    //
-    // console.log(venticinque_numeri);
-
-
-    // for (var i = 1; i < 25; i++) {
-    //     var numero = genera_random(1, 10);
-    //     $('.quadrato').text(numero);
-    // }
-
     $('.quadrato').each(function(){
-            var numero = genera_random(1, 10);
+            var numero = genera_random(0, 10);
             if (numero % 2) {
                 $(this).text(numero).addClass('rosso')
+            } else if (numero == 0){
+                $(this).text(numero).addClass('verde')
             } else {
                 $(this).text(numero);
             }
