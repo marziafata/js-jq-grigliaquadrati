db7687a5a6afbff76a3e448638e7e1acf6bd62b3
Marzia Fatarella <marziafatarella@gmail.com>
2020-05-08 09:17:15 +0200

    creata seconda griglia con jQuery

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ $(document).ready(function() {
     // BONUS 1: generare la griglia con jQuery
     // BONUS 2: se il numero da inserire è pari scriverlo in rosso, se è dispari in nero, se è 0 in verde
 
+    for (var i = 0; i < 25; i++) {
+        $('#griglia2').append('<div class="quadrato"></div>');
+    }
 
     $('.quadrato').each(function(){
             var numero = genera_random(0, 10);
